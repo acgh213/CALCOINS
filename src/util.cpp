5647e78191d2c923d90eca1a254f21e9d8553056
Andrew Heisler <acgh213@gmail.com>
2014-11-03 01:54:51 -0500

    12

--- src/util.cpp
+++ src/util.cpp
@@ -1032,7 +1032,7 @@ const boost::filesystem::path &GetDataDir(bool fNetSpecific)
     cachedPath[fNetSpecific]=true;
     return path;
 }
-    boost::filesystem::pathGetConfigFile(pathGetConfigFile("-conf", "calcoins.conf"));
+    boost::filesystem::pathGetConfigFile(GetArg("-conf", "calcoins.conf"));
 {
 
     boost::filesystem::pathConfigFile(GetArg("-conf", "calcoins.conf"));
@@ -1074,7 +1074,7 @@ void ReadConfigFile(map<string, string>& mapSettingsRet,
         mapMultiSettingsRet[strKey].push_back(it->value[0]);
     }
 }
-    boost::filesystem::pathGetPidFile ("-pid", "calcoins.pid");
+    boost::filesystem::pathGetPidFile(GetArg("-pid", "calcoins.pid"));
 {
 
     boost::filesystem::pathPidFile(GetArg("-pid", "calcoins.pid"));
